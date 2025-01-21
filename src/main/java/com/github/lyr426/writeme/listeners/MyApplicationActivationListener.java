3dce16f29c856d1a7bb5ea4c26d9ef05676b04d5
leeyuri <lyr426@etners.com>
2025-01-21 14:27:31 +0900

    feat: 초기 세팅 진행중

---
+++ src/main/java/com/github/lyr426/writeme/listeners/MyApplicationActivationListener.java
@@ -0,0 +1,16 @@
+package com.github.lyr426.writeme.listeners;
+
+import com.intellij.openapi.application.ApplicationActivationListener;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.wm.IdeFrame;
+import org.jetbrains.annotations.NotNull;
+
+public class MyApplicationActivationListener implements ApplicationActivationListener {
+
+    private static final Logger LOG = Logger.getInstance(MyApplicationActivationListener.class);
+
+    @Override
+    public void applicationActivated(@NotNull IdeFrame ideFrame) {
+        LOG.warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.");
+    }
+}
